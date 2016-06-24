c2ddd5ca892319fc0ff41cbc2f1b2e745a0d6048
msousinha <isabelcavalheiro@iMac-de-Isabel.local>
2016-06-24 11:50:26 +0100

    0.0.1

--- repo/plugin.video.SportsDevil/lib/utils/scrapingUtils.py
+++ repo/plugin.video.SportsDevil/lib/utils/scrapingUtils.py
@@ -136,15 +136,15 @@ def findFrames(data):
 
 def findContentRefreshLink(page, data):
     
-    regex = '0;\s*url=([^\'" ]+)'
+    regex = '0;\s*url=((?![^\'" ]+rojadirecta)[^\'" ]+)'
     links = regexUtils.findall(data, regex)
     if links:
         return links[0]
     
-    regex = 'window.location\s*=\s*[\'"]([^\'"]+)[\'"]'
-    links = regexUtils.findall(data, regex)
-    if links:
-        return links[0]
+    #regex = 'window.location\s*=\s*[\'"]([^\'"]+)[\'"]'
+    #links = regexUtils.findall(data, regex)
+    #if links:
+        #return links[0]
     
     regex = 'frame\s*scrolling=\"auto\"\s*noresize\s*src\s*=\s*[\'"]([^\'"]+)[\'"]'
     links = regexUtils.findall(data, regex)
@@ -179,7 +179,7 @@ def findVideoFrameLink(page, data):
     if not frames:
         return None
     
-    iframes = regexUtils.findall(data, "(frame(?![^>]*cbox\.ws)(?![^>]*Publi)(?![^>]*dailymotion)(?![^>]*blacktvlive\.)(?![^>]*chat\d*\.\w+)(?![^>]*ad122m)(?![^>]*adshell)(?![^>]*capacanal)(?![^>]*waframedia)(?![^>]*Beba.tv/embed)(?![^>]*maxtags)(?![^>]*s/a1\.php)(?![^>]*right-sidebar)[^>]*\sheight\s*=\s*[\"']*([\%\d]+)(?:px)?[\"']*[^>]*>)")
+    iframes = regexUtils.findall(data, "(frame(?![^>]*cbox\.ws)(?![^>]*Publi)(?![^>]*dailymotion)(?![^>]*guide\.)(?![^>]*chat\d*\.\w+)(?![^>]*ad122m)(?![^>]*adshell)(?![^>]*capacanal)(?![^>]*waframedia)(?![^>]*Beba.tv/embed)(?![^>]*maxtags)(?![^>]*s/a1\.php)(?![^>]*right-sidebar)[^>]*\sheight\s*=\s*[\"']*([\%\d]+)(?:px)?[\"']*[^>]*>)")
 
     if iframes:
         for iframe in iframes:
@@ -188,7 +188,7 @@ def findVideoFrameLink(page, data):
             else:
                 height = int(iframe[1])
             if height > minheight:
-                m = regexUtils.findall(iframe[0], "[\"' ]width\s*=\s*[\"']*(\d+[%]*)(?:px)?[\"']*")
+                m = regexUtils.findall(iframe[0], "[\"'\s]width\s*=\s*[\"']*(\d+[%]*)(?:px)?[\"']*")
                 if m:
                     if m[0] == '100%':
                         width = minwidth+1
@@ -222,6 +222,11 @@ def findVideoFrameLink(page, data):
         return urlparse.urljoin(urllib.unquote(page), m[0]).strip()
     
     m = regexUtils.findall(data, r'playStream\(\'iframe\', \'[^\']*(https*:[^\']+)\'\)')
+    if m:
+        return urlparse.urljoin(urllib.unquote(page), m[0]).strip()
+    
+    #sportsh**tv
+    m = regexUtils.findall(data, r'<iframe\s*src="(stream[^"]+)"\s*allowfullscreen>')
     if m:
         return urlparse.urljoin(urllib.unquote(page), m[0]).strip()
 
